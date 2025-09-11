32ecf8dfb67fc36e92516c312fedc74a7cf28ce6
prPedr <prpedro025@gmail.com>
2025-09-11 05:01:17 -0300

    Adicionada criptografia de senhas com bcrypt

--- src/service/usuarioServices.js
+++ src/service/usuarioServices.js
@@ -1,11 +1,18 @@
 import usuarioRepositories from "../repositories/usuarioRepositories.js"
+import bcrypt from "bcrypt"
 
 async function criarUsuarioServices(novoUsuario) {
     const procurarUsuarioEmail = await usuarioRepositories.procurarUsuarioEmailRepositories(novoUsuario.email)
     if (procurarUsuarioEmail) {
         throw new Error("Usuario ja existe no sistema, favor conferir e tentar novamente")
     }
-    const usuario = await usuarioRepositories.criarUsuarioRepositories(novoUsuario)
+
+    const senhaHash = await bcrypt.hash(novoUsuario.senha, 10)
+    const usuario = await usuarioRepositories.criarUsuarioRepositories({...novoUsuario, senha: senhaHash})
+    if (!usuario) {
+        throw new Error("Falha ao criar o usuario")
+    }
+
     return usuario
 }
 
